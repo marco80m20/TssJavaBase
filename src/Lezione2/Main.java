59005a02fa6e1429d9ad86aa97c67e7e785893fc
tss <tss@debian-tss24>
2016-11-14 11:12:33 +0100

    Lezione 2 creato package

---
+++ src/Lezione2/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lezione2;
+
+/**
+ * Esempio di commenti java
+ * 
+ * @author tss
+ */
+public class Main {
+    
+    /** da questo metodo partirà l'esecuzione del programma
+    */
+    public static void main(String[] args) {
+        
+    
+    // commento
+    /* commento su più righe
+    sdgsdgsdgsdgs
+    sggsgsgsdsdg
+    */
+    /** commento multiriga che si mette alla classe e a ciscun metodo
+    */
+    System.out.println ("Start esempio commenti");
+    }
+    
+}
+    
+    
+
