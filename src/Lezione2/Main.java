12a0269c4b47ef88a010c1dfe18627b62fc1d2d9
tss <tss@debian-tss24>
2016-11-14 12:33:01 +0100

    Lezione 2 creato package

--- src/Lezione2/Main.java
+++ src/Lezione2/Main.java
@@ -7,27 +7,44 @@ package Lezione2;
 
 /**
  * Esempio di commenti java
- * 
+ *
  * @author tss
  */
 public class Main {
-    
-    /** da questo metodo partirà l'esecuzione del programma
-    */
+    // variabile statica
+
+    private static int colore;
+    // variabile di istanza 
+    private int carattere;
+
+    /**
+     * da questo metodo partirà l'esecuzione del programma
+     */
     public static void main(String[] args) {
-        
-    
-    // commento
-    /* commento su più righe
+
+        // commento
+        /* commento su più righe
     sdgsdgsdgsdgs
     sggsgsgsdsdg
-    */
-    /** commento multiriga che si mette alla classe e a ciscun metodo
-    */
-    System.out.println ("Start esempio commenti");
-    }
-    
-}
-    
-    
+         */
+        /**
+         * commento multiriga che si mette alla classe e a ciscun metodo
+         */
+        System.out.println("Parametri in ingresso" + args);
 
+        System.out.println("Start esempio commenti");
+
+        // variabile locale
+        int totale = 10;
+
+        if (totale > 0) {
+            //variabile locale
+            int x = 5;
+
+        } else {
+            int x = 10;
+
+        }
+    }
+
+}
